0a8d1b3f9c2a9bf6595c5aff2108d5cc3a95389d
Bohdan <bohdan.shurlo@gmail.com>
2019-05-17 18:15:19 +0300

    Solution

--- config/backstop/backstopConfig.js
+++ config/backstop/backstopConfig.js
@@ -3,7 +3,7 @@
 // https://github.com/garris/BackstopJS#advanced-scenarios
 const basicScenario = {
   label: 'test', // name of the test
-  url: 'http://localhost:8080',
+  url: 'http://localhost:8080/index.html',
   referenceUrl: 'https://mate-academy.github.io/layout_solutions/moyo-header/',
   readyEvent: '',
   readySelector: '',
@@ -17,48 +17,46 @@ const basicScenario = {
   selectorExpansion: true,
   expect: 0,
   misMatchThreshold: 1, // 1% of 100%
-  requireSameDimensions: true
+  requireSameDimensions: true,
 };
 
 module.exports = {
   id: 'test',
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
-  viewports: [
-    {
-      name: 'desktop_s',
-      width: 1024,
-      height: 768
-    },
-    {
-      name: 'desktop_m',
-      width: 1200,
-      height: 768
-    }
+  viewports: [{
+    name: 'desktop_s',
+    width: 1024,
+    height: 768,
+  },
+  {
+    name: 'desktop_m',
+    width: 1200,
+    height: 768,
+  },
   ],
-  scenarios: [
-    {
-      ...basicScenario,
-      label: 'header-tag',
-      selectors: ['header']
-    },
-    {
-      ...basicScenario,
-      label: 'nav-tag',
-      selectors: ['nav']
-    },
-    {
-      ...basicScenario,
-      label: 'nav-item-hovered',
-      selectors: ['[data-qa="hover"]'],
-      hoverSelector: '[data-qa="hover"]',
-      postInteractionWait: 1000
-    },
-    {
-      ...basicScenario,
-      label: 'nav-item-active',
-      selectors: ['a.is-active']
-    }
+  scenarios: [{
+    ...basicScenario,
+    label: 'header-tag',
+    selectors: ['header'],
+  },
+  {
+    ...basicScenario,
+    label: 'nav-tag',
+    selectors: ['nav'],
+  },
+  {
+    ...basicScenario,
+    label: 'nav-item-hovered',
+    selectors: ['[data-qa="hover"]'],
+    hoverSelector: '[data-qa="hover"]',
+    postInteractionWait: 1000,
+  },
+  {
+    ...basicScenario,
+    label: 'nav-item-active',
+    selectors: ['a.is-active'],
+  },
     // define here scenarios for testing
   ],
   paths: {
@@ -66,15 +64,15 @@ module.exports = {
     bitmaps_test: 'backstop_data/bitmaps_test',
     engine_scripts: 'backstop_data/engine_scripts',
     html_report: 'backstop_data/html_report',
-    ci_report: 'backstop_data/ci_report'
+    ci_report: 'backstop_data/ci_report',
   },
   report: ['browser'],
   engine: 'puppeteer',
   engineOptions: {
-    args: ['--no-sandbox']
+    args: ['--no-sandbox'],
   },
   asyncCaptureLimit: 5,
   asyncCompareLimit: 50,
   debug: false,
-  debugWindow: false
+  debugWindow: false,
 };
