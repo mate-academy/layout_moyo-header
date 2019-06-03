59105c0f391e758568aa9b2652e547eff2d3fdc6
Dima Pankiv <pankivluck@gmail.com>
2019-06-04 00:25:17 +0300

    Solution

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const ulist = document.querySelector('.menu');
+const a = document.querySelectorAll('A');
+ulist.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName !== 'A' || target.hasAttribute('class')) {
+    return;
+  }
+  a.forEach((el) => {
+    el.removeAttribute('class');
+    target.setAttribute('class', 'is-active');
+  });
+});
