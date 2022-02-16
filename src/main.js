d0fb68adcb79204b8fc506154b5f752387340d5d
yurii837 <gorogotskiy@gmail.com>
2022-02-16 22:41:37 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const headerL = document.getElementById('largeNav');
+
+const btnsL = headerL.getElementsByClassName('ulL');
+
+for (let i = 0; i < btnsL.length; i++) {
+  btnsL[i].addEventListener('click', function() {
+    const currentL = document.getElementsByClassName('is-activeL');
+
+    if (currentL.length > 0) {
+      currentL[0].className = currentL[0].className.replace(' is-activeL', '');
+    }
+    this.className += ' is-activeL';
+  });
+}
+
+const headerS = document.getElementById('shortNav');
+
+const btnsS = headerS.getElementsByClassName('ulS');
+
+for (let i = 0; i < btnsS.length; i++) {
+  btnsS[i].addEventListener('click', function() {
+    const currentS = document.getElementsByClassName('is-activeS');
+
+    if (currentS.length > 0) {
+      currentS[0].className = currentS[0].className.replace(' is-activeS', '');
+    }
+    this.className += ' is-activeS';
+  });
+}
