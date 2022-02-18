65dac1478e1b2e9b59f161ef3adf02987a74051d
yurii837 <gorogotskiy@gmail.com>
2022-02-18 12:00:34 +0200

    0fixed

--- src/main.js
+++ src/main.js
@@ -2,30 +2,26 @@
 
 const headerL = document.getElementById('largeNav');
 
-const btnsL = headerL.getElementsByClassName('ulL');
+const btnsL = headerL.getElementsByClassName('a');
 
 for (let i = 0; i < btnsL.length; i++) {
   btnsL[i].addEventListener('click', function() {
     const currentL = document.getElementsByClassName('is-activeL');
 
-    if (currentL.length > 0) {
-      currentL[0].className = currentL[0].className.replace(' is-activeL', '');
-    }
+    currentL[0].className = currentL[0].className.replace(' is-activeL', '');
     this.className += ' is-activeL';
   });
 }
 
 const headerS = document.getElementById('shortNav');
 
-const btnsS = headerS.getElementsByClassName('ulS');
+const btnsS = headerS.getElementsByClassName('a');
 
 for (let i = 0; i < btnsS.length; i++) {
   btnsS[i].addEventListener('click', function() {
     const currentS = document.getElementsByClassName('is-activeS');
 
-    if (currentS.length > 0) {
-      currentS[0].className = currentS[0].className.replace(' is-activeS', '');
-    }
+    currentS[0].className = currentS[0].className.replace(' is-activeS', '');
     this.className += ' is-activeS';
   });
 }
