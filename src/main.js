a9ec6d3cead424d8b199a18c2eccc0c8b82dee3d
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-10-14 02:34:35 +0300

    added Moyo header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const itemsElement = document.getElementsByClassName('nav-list__item');
+const currentPath = window.location.pathname;
+
+Array.from(itemsElement).forEach(element => {
+  if (element.children[0].getAttribute('href') === currentPath) {
+    element.classList.add('is-active');
+  }
+});
