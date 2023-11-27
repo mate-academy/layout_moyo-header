c10b1129a1c575edaa8e920ae7952e555f5c1d6d
Artem <artemmalikov742@gmail.com>
2023-11-27 14:32:45 +0200

    add all files

--- backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
+++ backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
@@ -7,31 +7,31 @@ module.exports = async (page, scenario) => {
 
   if (keyPressSelector) {
     for (const keyPressSelectorItem of [].concat(keyPressSelector)) {
-      await page.waitForSelector(keyPressSelectorItem.selector);
+      await page.waitFor(keyPressSelectorItem.selector);
       await page.type(keyPressSelectorItem.selector, keyPressSelectorItem.keyPress);
     }
   }
 
   if (hoverSelector) {
     for (const hoverSelectorIndex of [].concat(hoverSelector)) {
-      await page.waitForSelector(hoverSelectorIndex);
+      await page.waitFor(hoverSelectorIndex);
       await page.hover(hoverSelectorIndex);
     }
   }
 
   if (clickSelector) {
     for (const clickSelectorIndex of [].concat(clickSelector)) {
-      await page.waitForSelector(clickSelectorIndex);
+      await page.waitFor(clickSelectorIndex);
       await page.click(clickSelectorIndex);
     }
   }
 
   if (postInteractionWait) {
-    await page.waitForTimeout(postInteractionWait);
+    await page.waitFor(postInteractionWait);
   }
 
   if (scrollToSelector) {
-    await page.waitForSelector(scrollToSelector);
+    await page.waitFor(scrollToSelector);
     await page.evaluate(scrollToSelector => {
       document.querySelector(scrollToSelector).scrollIntoView();
     }, scrollToSelector);
